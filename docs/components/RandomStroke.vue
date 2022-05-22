cbf800aadb057001a7ab5d7045ee001c27f70bcb
Liszt-Fly <1904231450@qq.com>
2022-05-22 15:28:45 +0800

    ⚡️ feat: RandomStroke组件开发

---
+++ docs/components/RandomStroke.vue
@@ -0,0 +1,20 @@
+<template>
+    <span class="container" ref="emph">属于真的了</span>
+</template>
+
+<script lang="ts" setup>
+import { getColor } from '../utils/colors';
+import { onMounted, ref } from 'vue';
+const emph = ref<HTMLSpanElement | null>()
+onMounted(() => {
+    const color = getColor()
+    emph.value!.style.color = color.code
+})
+</script>
+
+<style  scoped>
+.container {
+    font-weight: bold;
+    text-decoration: underline;
+}
+</style>
