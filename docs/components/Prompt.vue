e703a49edfeb8859dc77bcec747329945f326993
Liszt-Fly <1904231450@qq.com>
2022-05-23 19:48:52 +0800

    ⚡️ feat: PromptBlock提示组件开发

---
+++ docs/components/Prompt.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="prompt-container">
+        <svg class="icon" aria-hidden="true" width="20" height="20">
+            <use :xlink:href="'#' + getCode(item!).code"></use>
+        </svg>
+        <span class="content">
+            <slot></slot>
+        </span>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { getCode } from "../utils/IconSet"
+
+const props = defineProps({
+    item: String
+})
+</script>
+
+<style >
+.prompt-container {
+    background-color: rgb(235, 235, 235);
+    padding: 5px;
+
+}
+
+svg,
+span {
+    margin-right: 10px;
+    vertical-align: middle;
+}
+</style>
