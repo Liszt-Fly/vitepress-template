22b453d3c20610bbc572c31bca960bf9b9558482
Liszt-Fly <1904231450@qq.com>
2022-05-24 15:10:50 +0800

    ⚡️ feat: MindMap组件初步开发

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,12 +1,16 @@
 import DefaultTheme from "vitepress/theme"
+import "jsmind/style/jsmind.css"
 import RandomEmphasis from "@/RandomEmphasis.vue"
 import "./custom.css"
+import "//at.alicdn.com/t/font_3421024_62h59hodvju.js"
 import { autoComponents } from "../../utils/autocomponent"
+
 const object = ["RandomEmphasis", RandomEmphasis]
 export default {
 	...DefaultTheme,
 	enhanceApp({ app }) {
 		// register global components
+
 		autoComponents().forEach((vuecomponent) => {
 			console.log(vuecomponent.module.default)
 			app.component(vuecomponent.name, vuecomponent.module.default)
---
+++ docs/components/MindMap.vue
@@ -0,0 +1,59 @@
+<template>
+    <div id="jsmind_container"></div>
+</template>
+
+<script lang="ts" setup>
+//@ts-ignore
+import jsMind from "jsmind"
+
+import { onMounted } from "vue";
+
+function load_jsmind() {
+    var mind = {
+        "meta": {
+            "name": "demo",
+            "author": "mikeedu",
+            "version": "0.2",
+
+        },
+        "format": "node_array",
+        "data": [
+            { "id": "root", "isroot": true, "topic": "jsMind" },
+
+            { "id": "sub1", "parentid": "root", "topic": "sub1", },
+            { "id": "sub11", "parentid": "sub1", "topic": "sub11" },
+            { "id": "sub12", "parentid": "sub1", "topic": "sub12" },
+            { "id": "sub13", "parentid": "sub1", "topic": "sub13" },
+
+            { "id": "sub2", "parentid": "root", "topic": "sub2" },
+            { "id": "sub21", "parentid": "sub2", "topic": "sub21" },
+            { "id": "sub22", "parentid": "sub2", "topic": "sub22", },
+
+            { "id": "sub3", "parentid": "root", "topic": "sub3" },
+        ]
+    };
+    var options = {
+        container: 'jsmind_container',
+        editable: true,
+        "theme": "asphalt"
+    }
+    var jm = jsMind.show(options, mind);
+
+}
+
+
+onMounted(() => {
+    load_jsmind();
+
+
+})
+</script>
+
+<style lang="scss" scoped>
+#jsmind_container {
+    max-width: 800px;
+    height: 500px;
+    border: solid 1px #ccc;
+    background: #f4f4f4;
+}
+</style>
