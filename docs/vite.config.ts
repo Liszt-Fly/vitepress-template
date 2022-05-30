725ad35b9d6909e8a8b81d169cf9155a0191adfe
Liszt-Fly <1904231450@qq.com>
2022-05-30 11:06:21 +0800

    🎉 update: path-browserify添加依赖

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,4 +1,4 @@
-import path from 'path'
+import path from "path-browserify"
 import { foreach, root } from "./utils/foeach"
 import { defineConfig } from 'vite'
 console.log("应用配置生效")
@@ -6,6 +6,6 @@ foreach(root)
 console.log(path.resolve(__dirname, "components"))
 export default defineConfig({
     resolve: {
-        alias: { "@": path.resolve(__dirname, "components") }
+        alias: { "@": path.resolve(__dirname, "components"), path: "path-browserify" }
     }
 })
