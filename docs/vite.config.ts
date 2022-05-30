0341baf7eec34f9dcb17a8a3c62d50a034e88623
Liszt-Fly <1904231450@qq.com>
2022-05-30 15:23:00 +0800

    ⛏ refractor: 删除多余的console.log

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,7 +3,7 @@ import { foreach, root } from "./utils/foeach"
 import { defineConfig } from 'vite'
 console.log("应用配置生效")
 foreach(root)
-console.log(path.resolve(__dirname, "components"))
+
 export default defineConfig({
     resolve: {
         alias: { "@": path.resolve(__dirname, "components"), path: "path-browserify" }
