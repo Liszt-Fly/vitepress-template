d1f3e929f119e687101e3e22b04b24e7d1eacee4
Liszt-Fly <1904231450@qq.com>
2022-06-01 18:53:45 +0800

    🎉 update: 重新对navbar和guidebar进行重新布局和配置

--- docs/utils/foeach.ts
+++ docs/utils/foeach.ts
@@ -9,7 +9,6 @@ import { matter_home_structure } from '../interfaces/index_type'
 export const root = path.resolve(basePath, "docs", "target")
 const index_file = path.resolve(basePath, "docs", "index.md")
 export function foreach(folder: string) {
-    console.log(path.resolve(root))
     const content = fsp.readFileSync(index_file)
     console.log(content)
     const object = matter(content) as unknown as matter_home_structure
